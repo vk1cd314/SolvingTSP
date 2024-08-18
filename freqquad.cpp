c1ff78348a33fa7a55cd25ca88f565a555f13e08
Niloy <drakensang47@gmail.com>
2024-08-18 21:29:49 +0600

    Faster Faster Faster, gphash is worse idk why

--- freqquad.cpp
+++ freqquad.cpp
@@ -1,69 +1,33 @@
 #include <bits/stdc++.h>
-#include <chrono>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
 const int N = 1e7;
 
+pair <int, int> smol(int a, int b) {
+  if (a > b) swap(a, b);
+  return {a, b};
+}
+
 int get_freq(int a, int b, int c, int d,
-                                     map<pair<int, int>, int> &edgeList) {
-  // map<pair<int, int>, int> freq_dict;
+              map<pair<int, int>, int> &edgeList) {
 
-  double ab = edgeList[{a, b}] + rand() % N / (1.0 * N);
-  double bc = edgeList[{b, c}] + rand() % N / (1.0 * N);
-  double cd = edgeList[{c, d}] + rand() % N / (1.0 * N);
-  double ad = edgeList[{a, d}] + rand() % N / (1.0 * N);
-  double ac = edgeList[{a, c}] + rand() % N / (1.0 * N);
-  double bd = edgeList[{b, d}] + rand() % N / (1.0 * N);
+  double ab = edgeList[smol(a, b)] + rand() % N / (1.0 * N);
+  double bc = edgeList[smol(b, c)] + rand() % N / (1.0 * N);
+  double cd = edgeList[smol(c, d)] + rand() % N / (1.0 * N);
+  double ad = edgeList[smol(a, d)] + rand() % N / (1.0 * N);
+  double ac = edgeList[smol(a, c)] + rand() % N / (1.0 * N);
+  double bd = edgeList[smol(b, d)] + rand() % N / (1.0 * N);
 
-  if (ab + cd < ac + bd && ac + bd < ad + bc) {
-    return 5;
-    // freq_dict[{a, b}] = 5;
-    // freq_dict[{b, c}] = 1;
-    // freq_dict[{c, d}] = 5;
-    // freq_dict[{a, d}] = 1;
-    // freq_dict[{a, c}] = 3;
-    // freq_dict[{b, d}] = 3;
-  } else if (ab + cd < ad + bc && ad + bc < ac + bd) {
-    return 5;
-    // freq_dict[{a, b}] = 5;
-    // freq_dict[{b, c}] = 3;
-    // freq_dict[{c, d}] = 5;
-    // freq_dict[{a, d}] = 3;
-    // freq_dict[{a, c}] = 1;
-    // freq_dict[{b, d}] = 1;
-  } else if (ac + bd < ab + cd && ab + cd < ad + bc) {
-    return 3;
-    // freq_dict[{a, b}] = 3;
-    // freq_dict[{b, c}] = 1;
-    // freq_dict[{c, d}] = 3;
-    // freq_dict[{a, d}] = 1;
-    // freq_dict[{a, c}] = 5;
-    // freq_dict[{b, d}] = 5;
-  } else if (ac + bd < ad + bc && ad + bc < ab + cd) {
-    return 1;
-    // freq_dict[{a, b}] = 1;
-    // freq_dict[{b, c}] = 3;
-    // freq_dict[{c, d}] = 1;
-    // freq_dict[{a, d}] = 3;
-    // freq_dict[{a, c}] = 5;
-    // freq_dict[{b, d}] = 5;
-  } else if (ad + bc < ab + cd && ab + cd < ac + bd) {
-    return 3;
-    // freq_dict[{a, b}] = 3;
-    // freq_dict[{b, c}] = 5;
-    // freq_dict[{c, d}] = 3;
-    // freq_dict[{a, d}] = 5;
-    // freq_dict[{a, c}] = 1;
-    // freq_dict[{b, d}] = 1;
-  } else if (ad + bc < ac + bd && ac + bd < ab + cd) {
-    return 1;
-    // freq_dict[{a, b}] = 1;
-    // freq_dict[{b, c}] = 5;
-    // freq_dict[{c, d}] = 1;
-    // freq_dict[{a, d}] = 5;
-    // freq_dict[{a, c}] = 3;
-    // freq_dict[{b, d}] = 3;
-  }
+  if (ab + cd < ac + bd && ac + bd < ad + bc) return 5;
+  if (ab + cd < ad + bc && ad + bc < ac + bd) return 5;
+  if (ac + bd < ab + cd && ab + cd < ad + bc) return 3;
+  if (ac + bd < ad + bc && ad + bc < ab + cd) return 1;
+  if (ad + bc < ab + cd && ab + cd < ac + bd) return 3;
+  if (ad + bc < ac + bd && ac + bd < ab + cd) return 1;
 
   return 0;
 }
@@ -79,56 +43,47 @@ int main(int argc, char **argv) {
   int length;
 
   map<pair<int, int>, int> edge_map;
+  vector <array <int, 3>> edge_list;
   while (file >> v1 >> v2 >> length) {
+    if (v1 > v2) swap(v1, v2);
     edge_map[{v1, v2}] = length;
-    edge_map[{v2, v1}] = length;
+    edge_list.push_back({v1, v2, 0});
   }
   file.close();
-  auto start = std::chrono::high_resolution_clock::now();
-  map<pair<int, int>, int> edge_freq;
-  map<pair<int, int>, bool> done;
-  for (auto [edge, len] : edge_map) {
+
+  auto start = chrono::high_resolution_clock::now();
+  
+  for (int i = 0; i < (int) edge_list.size(); ++i) {
     int edge_freq_e = 0;
-    auto [u, v] = edge;
-    if (done[{min(u, v), max(u, v)}] == 1)
-      continue;
-    done[{min(u, v), max(u, v)}] = 1;
-    map<tuple<int, int, int, int>, bool> checked;
-    vector<pair<int, int>> freq_quads;
-    for (int i = 0; i < vertices; ++i) {
+    auto &[u, v, avg_freq] = edge_list[i];
+
+    for (int i = 0; i < vertices; i++) {
       int u1 = rand() % vertices;
       while (u1 == u || u1 == v)
         u1 = rand() % vertices;
       int v1 = rand() % vertices;
       while (v1 == u || v1 == v || u1 == v1)
         v1 = rand() % vertices;
-      freq_quads.push_back({u1, v1});
-    }
-    for (int i = 0; i < vertices; i++) {
-      auto [u1, v1] = freq_quads[i];
-      // auto freq_dict = get_freq(u, v, u1, v1, edge_map);
-      // edge_freq_e += freq_dict[{u, v}];
       edge_freq_e += get_freq(u, v, u1, v1, edge_map);
     }
-    edge_freq[{u, v}] = 1.0 * edge_freq_e / vertices;
+
+    avg_freq = 1.0 * edge_freq_e / vertices;
   }
-  vector<tuple<int, int, int>> sorted_edges;
-  for (auto [edge, len] : edge_freq)
-    sorted_edges.push_back({get<0>(edge), get<1>(edge), len});
-  sort(sorted_edges.begin(), sorted_edges.end(),
-       [](tuple<int, int, int> a, tuple<int, int, int> b) {
-         return get<2>(a) > get<2>(b);
+
+  sort(edge_list.begin(), edge_list.end(),
+       [](array<int, 3> a, array<int, 3> b) {
+         return a[2] > b[2];
        });
-  auto ed = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(ed - start);
+  
+  auto ed = chrono::high_resolution_clock::now();
+  auto duration = chrono::duration_cast<chrono::microseconds>(ed - start);
   cerr << "Time taken: " << duration.count() / 1000000.0 << " seconds" << '\n';
   cout << vertices << " " << (2 * edges + 2) / 3 << '\n';
-  vector<tuple<int, int, int>> to_write;
+ 
   for (int i = 0; i < (2 * edges + 2) / 3; i++) {
-    int u = get<0>(sorted_edges[i]);
-    int v = get<1>(sorted_edges[i]);
+    int u = edge_list[i][0]; 
+    int v = edge_list[i][1];
     int w = edge_map[{u, v}];
     cout << u << " " << v << " " << w << '\n';
   }
-  return 0;
 }
--- runcpp.sh
+++ runcpp.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 g++ gen_random_graph.cpp && ./a.out $1 $2 > input_graph.txt
 g++ freqquad.cpp && ./a.out input_graph.txt > output_graph.txt
-echo "Results for Original Graph:"
-./concorde-bin -x -N 10 input_graph.txt | grep 'Optimal Solution'
-echo "Results for Sparsified Graph:"
-./concorde-bin -x -N 10 output_graph.txt | grep 'Optimal Solution'
+
+RED='\033[31m'
+NC='\033[0m' 
+
+echo -e "${RED}Results for Original Graph:${NC}"
+time ./concorde-bin -x -N 10 input_graph.txt | grep 'Optimal Solution'
+
+echo -e "${RED}Results for Sparsified Graph:${NC}"
+time ./concorde-bin -x -N 10 output_graph.txt | grep 'Optimal Solution'
+
