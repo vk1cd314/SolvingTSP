4f5ee5b2a782146d4a1e82fd2b71baa250fec0df
Niloy <drakensang47@gmail.com>
2024-08-18 21:43:40 +0600

    Changed run script

--- runcpp.sh
+++ runcpp.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-g++ gen_random_graph.cpp && ./a.out $1 $2 > input_graph.txt
-g++ freqquad.cpp && ./a.out input_graph.txt > output_graph.txt
+g++ gen_random_graph.cpp -o gen_random_graph && ./gen_random_graph $1 $2 > input_graph.txt
+g++ freqquad.cpp -o freqquad && ./freqquad input_graph.txt > output_graph.txt
 
 RED='\033[31m'
+YELLOW='\033[33m'
 NC='\033[0m' 
 
-echo -e "${RED}Results for Original Graph:${NC}"
+echo -e "${RED}Results for ${YELLOW}Original${RED} Graph:${NC}"
 time ./concorde-bin -x -N 10 input_graph.txt | grep 'Optimal Solution'
 
-echo -e "${RED}Results for Sparsified Graph:${NC}"
+echo -e "${RED}Results for ${YELLOW}Sparsified${RED} Graph:${NC}"
 time ./concorde-bin -x -N 10 output_graph.txt | grep 'Optimal Solution'
+
+rm *input_graph*
+rm *output_graph*
+rm gen_random_graph freqquad
 
