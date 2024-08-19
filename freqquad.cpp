57a3dc7fb2c43940596981e3ea3ca5277bc8adad
Niloy <drakensang47@gmail.com>
2024-08-19 07:09:29 +0600

    modified:   freqquad.cpp -> Added multiple iterations made order of magnitude faster new file:   graph_out/output_graph.txt -> Saving outputs modified:   runcpp.sh -> Changed script to save outputs

--- freqquad.cpp
+++ freqquad.cpp
@@ -1,26 +1,27 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
-using namespace __gnu_pbds;
 
-const int N = 1e7;
+const int N = 1e7, M = 1e3 + 10;
+const int MX_REP = 1e8;
+int edge_map[M][M];
 
-pair <int, int> smol(int a, int b) {
-  if (a > b) swap(a, b);
-  return {a, b};
-}
-
-int get_freq(int a, int b, int c, int d,
-              map<pair<int, int>, int> &edgeList) {
-
-  double ab = edgeList[smol(a, b)] + rand() % N / (1.0 * N);
-  double bc = edgeList[smol(b, c)] + rand() % N / (1.0 * N);
-  double cd = edgeList[smol(c, d)] + rand() % N / (1.0 * N);
-  double ad = edgeList[smol(a, d)] + rand() % N / (1.0 * N);
-  double ac = edgeList[smol(a, c)] + rand() % N / (1.0 * N);
-  double bd = edgeList[smol(b, d)] + rand() % N / (1.0 * N);
+int get_freq(int a, int b, int c, int d) {
+  double ab = edge_map[a][b]; 
+  double bc = edge_map[b][c];
+  double cd = edge_map[c][d];
+  double ad = edge_map[a][d];
+  double ac = edge_map[a][c];
+  double bd = edge_map[b][d];
+  
+  if (ab == N or bc == N or cd == N or ad == N) return -1;
+  
+  ab += rand() % N / (1.0 * N); 
+  bc += rand() % N / (1.0 * N);
+  cd += rand() % N / (1.0 * N);
+  ad += rand() % N / (1.0 * N);
+  ac += rand() % N / (1.0 * N);
+  bd += rand() % N / (1.0 * N);
 
   if (ab + cd < ac + bd && ac + bd < ad + bc) return 5;
   if (ab + cd < ad + bc && ad + bc < ac + bd) return 5;
@@ -36,54 +37,82 @@ int main(int argc, char **argv) {
   srand(time(0));
   ifstream file;
   char *filename = argv[1];
+  int t_count = atoi(argv[2]);
   file.open(filename);
   int vertices, edges;
   file >> vertices >> edges;
   int v1, v2;
   int length;
 
-  map<pair<int, int>, int> edge_map;
+  const int QUAD_NO = min(vertices, 120);
+
   vector <array <int, 3>> edge_list;
   while (file >> v1 >> v2 >> length) {
     if (v1 > v2) swap(v1, v2);
-    edge_map[{v1, v2}] = length;
+    edge_map[v1][v2] = length;
+    edge_map[v2][v1] = length;
     edge_list.push_back({v1, v2, 0});
   }
   file.close();
 
   auto start = chrono::high_resolution_clock::now();
-  
-  for (int i = 0; i < (int) edge_list.size(); ++i) {
-    int edge_freq_e = 0;
-    auto &[u, v, avg_freq] = edge_list[i];
+ 
+  for (int turn = 0; turn < t_count; ++turn) {  
+    auto start_turn = chrono::high_resolution_clock::now();
+    bool ends = false;
+    for (int i = 0; i < (int) edge_list.size(); ++i) {
+      auto &[u, v, edge_freq_e] = edge_list[i];
 
-    for (int i = 0; i < vertices; i++) {
-      int u1 = rand() % vertices;
-      while (u1 == u || u1 == v)
-        u1 = rand() % vertices;
-      int v1 = rand() % vertices;
-      while (v1 == u || v1 == v || u1 == v1)
-        v1 = rand() % vertices;
-      edge_freq_e += get_freq(u, v, u1, v1, edge_map);
+      for (int i = 0; i < QUAD_NO; i++) {
+        int cnt = 0;
+      st:
+        cnt += 1;
+        if (cnt > MX_REP) {
+          ends = true;
+          break;
+        }
+        int u1 = rand() % vertices;
+        while (u1 == u || u1 == v)
+          u1 = rand() % vertices;
+        int v1 = rand() % vertices;
+        while (v1 == u || v1 == v || u1 == v1)
+          v1 = rand() % vertices;
+        int freq = get_freq(u, v, u1, v1);
+        if (freq == -1) goto st;
+        edge_freq_e += freq;
+      }
+    }
+    
+    if (ends) {
+      cerr << "broke at turn " << turn + 1 << '\n';
+      break;
     }
 
-    avg_freq = 1.0 * edge_freq_e / vertices;
+    vector <array<int, 3>> new_edge_list;
+    for (int i = 0; i < (int) edge_list.size(); ++i) {
+      if (edge_list[i][2] >= 3 * QUAD_NO) {
+        new_edge_list.push_back({edge_list[i][0], edge_list[i][1], 0});
+      } else {
+        edge_map[edge_list[i][0]][edge_list[i][1]] = N;
+        edge_map[edge_list[i][1]][edge_list[i][0]] = N;
+      }
+    }
+    edge_list = new_edge_list;
+    auto end_turn = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_turn - start_turn);
+    cerr << "Time need for turn " << turn + 1 << ": " << duration.count() / 1000000.0 << " seconds\n";
+    cerr << new_edge_list.size() << '\n';
   }
 
-  sort(edge_list.begin(), edge_list.end(),
-       [](array<int, 3> a, array<int, 3> b) {
-         return a[2] > b[2];
-       });
-  
-  auto ed = chrono::high_resolution_clock::now();
-  auto duration = chrono::duration_cast<chrono::microseconds>(ed - start);
-  cerr << "Time taken: " << duration.count() / 1000000.0 << " seconds" << '\n';
-  cout << vertices << " " << (2 * edges + 2) / 3 << '\n';
+  auto end = chrono::high_resolution_clock::now();
+  auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+  cerr << "Time taken total: " << duration.count() / 1000000.0 << " seconds" << '\n';
+  cout << vertices << " " << edge_list.size() << '\n';
  
-  for (int i = 0; i < (2 * edges + 2) / 3; i++) {
+  for (int i = 0; i < (int) edge_list.size(); i++) {
     int u = edge_list[i][0]; 
     int v = edge_list[i][1];
-    int w = edge_map[{u, v}];
+    int w = edge_map[u][v];
     cout << u << " " << v << " " << w << '\n';
   }
 }
--- runcpp.sh
+++ runcpp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 g++ gen_random_graph.cpp -o gen_random_graph && ./gen_random_graph $1 $2 > input_graph.txt
-g++ freqquad.cpp -o freqquad && ./freqquad input_graph.txt > output_graph.txt
+g++ freqquad.cpp -o freqquad && ./freqquad input_graph.txt $3 > output_graph.txt
 
 RED='\033[31m'
 YELLOW='\033[33m'
@@ -12,7 +12,9 @@ time ./concorde-bin -x -N 10 input_graph.txt | grep 'Optimal Solution'
 echo -e "${RED}Results for ${YELLOW}Sparsified${RED} Graph:${NC}"
 time ./concorde-bin -x -N 10 output_graph.txt | grep 'Optimal Solution'
 
+mv input_graph.txt graph_out/
 rm *input_graph*
+mv output_graph.txt graph_out/
 rm *output_graph*
 rm gen_random_graph freqquad
 
